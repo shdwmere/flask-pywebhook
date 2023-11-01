854560c4b8c6820036886a742c189b7d891f05ed
delphiu-m <caioszrm1@gmail.com>
2023-11-01 13:48:37 -0300

    added webhook route

--- main.py
+++ main.py
@@ -7,6 +7,20 @@ app = Flask(__name__)
 def index():
     return render_template('./test.html')
 
+# Rota para receber eventos do gateway de pagamento
+@app.route('/webhook', methods=['POST'])
+def webhook_listener():
+    # Capturando dados enviados no webhook
+    data = request.get_json()
+
+    # Processamento dos dados recebidos (apenas imprimir por enquanto)
+    print("Evento capturado:")
+    print(data)
+
+    # Aqui você pode adicionar a lógica para processar os eventos do gateway de pagamento. Por exemplo, atualizar um banco de dados, enviar notificações, etc.
+
+    # Responder ao gateway de pagamento indicando que o evento foi recebido com sucesso (código 200)
+    return '', 200
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
