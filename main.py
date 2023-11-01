8ab0e7d8edb40936a420e62fe9829340e2713363
delphiu-m <caioszrm1@gmail.com>
2023-11-01 13:58:28 -0300

    https request ok, handling logs

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+logs = []
+
 # Rota index
 @app.route('/')
 def index():
-    return render_template('./test.html')
+    return render_template('index.html')
 
 # Rota para receber eventos do gateway de pagamento
 @app.route('/webhook', methods=['POST'])
@@ -13,14 +15,18 @@ def webhook_listener():
     # Capturando dados enviados no webhook
     data = request.get_json()
 
-    # Processamento dos dados recebidos (apenas imprimir por enquanto)
-    print("Evento capturado:")
-    print(data)
+    # Capturando dados recebidos e jogando para um template logs
+    logs.append("Evento capturado:")
+    logs.append(str(data))
 
     # Aqui você pode adicionar a lógica para processar os eventos do gateway de pagamento. Por exemplo, atualizar um banco de dados, enviar notificações, etc.
 
     # Responder ao gateway de pagamento indicando que o evento foi recebido com sucesso (código 200)
     return '', 200
+
+@app.route('/logs')
+def show_logs():
+    return render_template('logs.html', logs=logs)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
