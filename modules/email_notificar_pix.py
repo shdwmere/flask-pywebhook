d888c5aa9718aacfb3df82d9520dba50265b6bfa
Caio Souza <123371625+delphiu-m@users.noreply.github.com>
2023-12-16 00:34:35 -0300

    logss

--- modules/email_notificar_pix.py
+++ modules/email_notificar_pix.py
@@ -19,24 +19,17 @@ def email_notificar_pix(email, nome, nome_loja, pix_code):
     
     try:
         print(f"\033[1;32m \n[+] Módulo de Notificação PIX carregando...\n\033[0m")
-        print(f"\033[0;35m Conectando ao servidor SMTP: '{host_smtp}'... \033[0m")
         server = smtplib.SMTP_SSL(host_smtp, port=465)
-
-        print(f"\033[0;35m Logando no e-mail: '{remetente}'... \033[0m")
         server.login(remetente, password)
-
-        print(f"\033[0;33m Carregando template HTML... \033[0m")
         msg = MIMEMultipart()
         msg['From'] = remetente
         msg['To'] = destinatario
         msg['Subject'] = assunto
         msg.attach(MIMEText(mensagem_html, 'html'))
 
-        print(f"\033[0;36m Enviando o e-mail... \033[0m")
         server.sendmail(remetente, destinatario, msg.as_string())
 
-
-        print(f'\033[1;32m E-mail enviado com sucesso. \033[0m')
+        print(f'\033[1;32m E-mail com codigo PIX enviado. \033[0m')
         server.quit()
     except smtplib.SMTPException as e:
         print(f'\033[1;31m Falha ao enviar o e-mail: {str(e)} \033[0m')
